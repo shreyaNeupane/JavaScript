dc50ab84fe071633d1698f33665d23535b74f886
Shreya <Shreyaneupane49@gmail.com>
2024-11-29 08:36:48 +0545

    changes in rest parameter

--- restParameters.js
+++ restParameters.js
@@ -5,19 +5,45 @@
 //                  rest = bundles seperate elements into an array
 
 
-function openFridge(...foods){
-    console.log(foods);
+// function openFridge(...foods){
+//     console.log(foods);
+// }
+// function getFood(...foods){
+//     return foods;
+// }
+
+// const food1 = "pizza";
+// const food2 = "hamburger";
+// const food3 = "hotdog";
+// const food4 = "sushi";
+
+// // openFridge(food1 , food2 , food3 , food4);
+
+// const foods = getFood(food1, food2, food3, food4);
+// console.log(foods);
+
+// ---------------To Find Sum ---------------------------
+function sum(...numbers){
+    let result = 0;
+
+    for(let number of numbers){
+        result +=number;
+    }
+    return result;
 }
-function getFood(...foods){
-    return foods;
+const total = sum(1,2,3);
+
+console.log(`your total id $${total}`);
+// ------------------ To Find Average ---------------------
+
+function average(...numbers) {
+  let result = 0;
+
+  for (let number of numbers) {
+    result += number;
+  }
+  return result/numbers.length;
 }
+const avg= average(1,2,3);
 
-const food1 = "pizza";
-const food2 = "hamburger";
-const food3 = "hotdog";
-const food4 = "sushi";
-
-// openFridge(food1 , food2 , food3 , food4);
-
-const foods = getFood(food1, food2, food3, food4);
-console.log(foods);
+console.log(`your average is ${avg}`);
