1f1053c6640b80409454c139b2955be3c0104492
shreyaNeupane <shreyaneupane49@gmail.com>
2024-12-27 11:24:37 +0545

    EVents

---
+++ keyEvent/index.js
@@ -0,0 +1,21 @@
+// eventListener = Listen for specific events to create interactive web pages
+//                  events: keydown, keyup  
+//                  document.addEventListener(event, callback);
+
+// let keyPressed = {};
+
+document.addEventListener("keydown", (event) => {
+ 
+    console.log(event);
+    
+});
+
+// document.addEventListener("keyup", (event) => {
+//   console.log(`key up = ${event.key}`);
+  
+// });
+// document.getElementById("output");
+// document.addEventListener("keydown" ,event => {
+//             const output = document.getElementById("output");
+//             output.textContent =`  Key: ${event.key}, Code: ${event.code}`;
+//         });
