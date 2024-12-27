409dfbc1ed9cc808a013cb02c5d6621e30c558bc
shreyaNeupane <shreyaneupane49@gmail.com>
2024-12-27 20:05:15 +0545

    Callback Hell

---
+++ callBackHell/index.js
@@ -0,0 +1,43 @@
+// Callback Hell = situation in javascript where callbacks 
+//                 are nested within other callbacks to the
+//                 degree where the code is difficult to read.
+//                 Old pattern to handle asynchronus functions.
+//                 use Promises + async/await to avoid callback Hell
+
+//Callbacks are useful for handling asynchronous operations, but they become confusing when nested deeply.
+
+function task1(callback){
+    setTimeout(() => {
+       console.log("Task 1 complete");
+       callback();
+    },2000);
+    
+}
+function task2(callback) {
+  setTimeout(() => {
+    console.log("Task 2 complete");
+     callback();
+  }, 1000);
+}
+function task3(callback) {
+  setTimeout(() => {
+    console.log("Task 3 complete");
+     callback();
+  }, 3000);
+}
+function task4(callback) {
+  setTimeout(() => {
+    console.log("Task 4 complete");
+     callback();
+  }, 1500);
+}
+// nesting callbacks inside of other callbacks => callback hell
+task1(() => {
+    task2(() => {
+       task3(() => {
+        task4(() =>  console.log("All task complete"));
+        })
+       })
+    })
+
+// console.log("All task complete");
